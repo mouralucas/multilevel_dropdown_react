51d80965c61fbf14506350dbf7459bfa0452015b
Lucas Moura <lucaspenha471@gmail.com>
2023-04-02 22:47:40 -0300

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import Header from "./Header";
+
+// Documentation:
+// https://blog.logrocket.com/how-create-multilevel-dropdown-menu-react/
+
+const App = () => {
+    return (
+        <div>
+            <Header />
+        </div>
+    );
+};
+
+export default App;
